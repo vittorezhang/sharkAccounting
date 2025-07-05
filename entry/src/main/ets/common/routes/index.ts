c6720e27087db5a98c7925876f120fe224456194
zwt <yc_zwt@163.com>
2025-07-05 14:04:18 +0800

    feat: 路由拦截添加白名单

---
+++ entry/src/main/ets/common/routes/index.ts
@@ -0,0 +1,12 @@
+export const PUBLIC_ROUTES = [
+  // 'pages/detail', // 明细页免校验
+  'pages/find'// 发现页免校验
+];
+
+export const TAB_ROUTES = [
+  'pages/detail',
+  'pages/chart',
+  'pages/account',
+  'pages/find',
+  'pages/me'
+];
